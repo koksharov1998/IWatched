28ea4bb7e3f2da761e90d6eaff3bf9bee46df4f3
koksharov1998 <koksharov1998@ya.ru>
2020-12-23 23:06:21 +0500

    Добавлена иконка и стили для home.html

--- src/main/java/com/example/IWatched/web/HelloController.java
+++ src/main/java/com/example/IWatched/web/HelloController.java
@@ -1,18 +1,42 @@
 package com.example.IWatched.web;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-  public HelloController() {
-
-  }
 
   @GetMapping("/hello")
   public String getHello() {
     return "hello";
   }
 
+
+  @RequestMapping(value = "/favicon.ico", method = RequestMethod.GET)
+  @ResponseBody
+  public ResponseEntity<InputStreamResource> downloadFavicon() throws IOException {
+
+    System.out.println("dssfs");
+    byte[] favicon;
+
+    favicon = Files.readAllBytes(Paths.get(
+        System.getProperty("user.dir") + "/src/main/resources/static/favicon.png"));
+
+    return ResponseEntity.ok()
+        .contentLength(favicon.length)
+        .contentType(MediaType.IMAGE_PNG)
+        .body(new InputStreamResource(new ByteArrayInputStream(favicon)));
+
+  }
 }
